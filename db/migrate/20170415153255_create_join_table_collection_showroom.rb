cd255bb073752b98b223d1389a8b476cbe0b3874
Mikhail <loskana2@mail.ru>
2017-04-28 00:18:41 +0300

    123

--- db/migrate/20170415153255_create_join_table_collection_showroom.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCollectionShowroom < ActiveRecord::Migration
-  def change
-    create_join_table :collections, :showrooms do |t|
-       t.index [:collection_id, :showroom_id]
-      # t.index [:showroom_id, :collection_id]
-    end
-  end
-end
